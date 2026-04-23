043bf970485db27fe672e380ccca5fb8f2371d61
Ronak Singh <titaniumqwertyuiop@gmail.com>
2026-04-23 23:17:42 +0530

    basic i/p o/p

---
+++ Inputs.java
@@ -0,0 +1,23 @@
+
+
+import java.util.*;
+
+public class Inputs{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        // System.out.println("Enter your roll no: ");
+        // int roll_no = sc.nextInt();
+        // System.out.println("Your roll no is " + roll_no);
+
+        // Underscore is replaced with comma { , }; used for representing millions
+        // int mil = 324_000_000;
+        // System.out.println(mil);
+        
+        String name = sc.nextLine();
+        System.out.println(name);
+
+        
+    }
+
+}
---
+++ data_types.java
@@ -0,0 +1,30 @@
+
+
+import java.util.*;
+
+public class data_types{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        // Primitive data types are those which cant be further broken into other data types.
+        System.out.println("Primitive Data Types");
+        int roll_no = 64;
+        long largeInteger = 3475816235985468946L;
+        char letter = 'r';
+        float decimal = 98.76f;
+        double largeDecimal = 45645454.465458;
+        boolean check = false;
+        System.out.println("int\t->\t"+roll_no);
+        System.out.println("long\t->\t"+largeInteger);
+        System.out.println("char\t->\t"+letter);
+        System.out.println("float\t->\t"+decimal);
+        System.out.println("double\t->\t"+largeDecimal);
+        System.out.println("boolean\t->\t"+check+"\n");
+        
+        System.out.println("Non Primitive Data Types");
+        String name = "Ronngh";
+        System.out.println("String\t->\t"+name);
+
+        
+    }
+}
